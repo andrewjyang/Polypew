c6d7be0ac049873d777db0cfed7cd84201872da2
Andrew Zenoni <azenoni@andrews-macbook-pro.local>
2018-12-06 20:23:51 -0800

    Added shapes and fixed issue with polygons disappearing. Started collisions

--- polypew/GameScene.swift
+++ polypew/GameScene.swift
@@ -22,10 +22,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     var spawnAstrogon: Timer!
-    var astrogons = ["hexagon"]
+    var astrogons = ["hexagon", "square", "triangle"]
     
-    let astrogonCategory: UInt32 = 0x1 << 1
-    let torpedoCategory: UInt32 = 0x1 << 0
+    
+    enum NodeCategory: UInt32 {
+        case player = 1
+        case astrogon = 2
+        case torpedo = 4
+        // unique powers of two because of bitwise and-ing and or-ing
+    }
     
     override func didMove(to view: SKView) {
         starfield = SKEmitterNode(fileNamed: "starfield")
@@ -37,6 +42,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player = SKSpriteNode(imageNamed: "spaceship")
         player.size = CGSize(width: 160, height: 140)
         player.position = CGPoint(x: self.frame.midX, y: self.frame.minY + player.size.height)
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: player.size.width, height: player.size.height))
+//        player.physicsBody?.contactTestBitMask = 
         self.addChild(player)
         
         self.physicsWorld.gravity = CGVector(dx:0, dy:0) // no effect of gravity in x or y direction
@@ -57,28 +64,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     @objc func addAstrogon() {
         astrogons = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: astrogons) as! [String]
-        let astrogon  = SKSpriteNode(imageNamed: astrogons[0])
-        astrogon.size = CGSize(width: 100, height: 100)
-        let astrogonPosition = GKRandomDistribution(lowestValue: -300, highestValue: 300)
+        let astrogon  = SKSpriteNode(imageNamed: astrogons.randomElement()!)
+        astrogon.size = CGSize(width: 84, height: 84)
+        let astrogonPosition = GKRandomDistribution(lowestValue: Int(self.frame.minX + astrogon.size.width), highestValue: Int(self.frame.maxX - astrogon.size.width))
         let position = CGFloat(astrogonPosition.nextInt())
         astrogon.position = CGPoint(x: position, y: self.frame.size.height + astrogon.size.height)
         
         astrogon.physicsBody?.isDynamic = true
-        astrogon.physicsBody?.categoryBitMask = astrogonCategory
-        astrogon.physicsBody?.contactTestBitMask = torpedoCategory
+        astrogon.physicsBody?.categoryBitMask = NodeCategory.astrogon.rawValue
+        astrogon.physicsBody?.contactTestBitMask = NodeCategory.torpedo.rawValue | NodeCategory.player.rawValue
         astrogon.physicsBody?.collisionBitMask = 0
         self.addChild(astrogon)
         let animationDuration: TimeInterval = 5
         
         var actions = [SKAction]()
-        actions.append(SKAction.move(to: CGPoint(x: position, y: -astrogon.size.height), duration: animationDuration))
+        actions.append(SKAction.move(to: CGPoint(x: position, y: self.frame.minY - astrogon.size.height), duration: animationDuration))
         actions.append(SKAction.removeFromParent())
 
         astrogon.run(SKAction.sequence(actions))
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let torpedo = SKSpriteNode(imageNamed: "torpedo")
+        torpedo.size = CGSize(width: 32, height: 32)
+        let position = self.player.position
+        let move = SKAction.move(to: CGPoint(x: position.x, y: self.frame.maxY + torpedo.size.height), duration: 5)
+        
+        torpedo.physicsBody = SKPhysicsBody(circleOfRadius: torpedo.size.height/2)
+        torpedo.physicsBody?.affectedByGravity = false
+        
+    }
+    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        // determining when object hit each other
     }
 }
